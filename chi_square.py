bc1e83adad5e0e828d116f684f37b39305a10a4d
Yasser KB <yasserkb@Yassers-MacBook-Pro.local>
2023-04-18 14:47:59 +0300

    First commit

---
+++ chi_square.py
@@ -0,0 +1,114 @@
+# import numpy as np
+# import scipy.stats as stats
+# import matplotlib.pyplot as plt
+#
+# # Generate data for normal distribution
+# mu, sigma = 0, 1
+# n = 1000
+# data_norm = np.random.normal(mu, sigma, n)
+#
+# # Generate data for exponential distribution
+# beta = 1
+# data_exp = np.random.exponential(beta, n)
+#
+# # Generate data for Poisson distribution
+# lam = 5
+# data_poisson = np.random.poisson(lam, n)
+#
+# # Generate data for gamma distribution
+# shape, scale = 2, 2
+# data_gamma = np.random.gamma(shape, scale, n)
+#
+# # Define bin edges and expected frequencies
+# bins = 20
+# norm_bins = np.linspace(-4, 4, bins+1)
+# exp_bins = np.linspace(0, 10, bins+1)
+# poisson_bins = np.arange(0, 20, 1)
+# gamma_bins = np.linspace(0, 20, bins+1)
+#
+# norm_expected = n*np.diff(stats.norm.cdf(norm_bins, mu, sigma))
+# exp_expected = n*np.diff(stats.expon.cdf(exp_bins, 0, beta))
+# poisson_expected = n*np.diff(stats.poisson.cdf(poisson_bins, lam))
+# gamma_expected = n*np.diff(stats.gamma.cdf(gamma_bins, shape, scale))
+#
+# # Perform chi-square goodness-of-fit test and plot results for each distribution
+# fig, axs = plt.subplots(2, 2, figsize=(12, 8))
+#
+# # Normal distribution
+# norm_observed, norm_bins, _ = axs[0, 0].hist(data_norm, bins=norm_bins, density=True, alpha=0.5)
+# norm_chi2, norm_p = stats.chisquare(norm_observed, norm_expected)
+# axs[0, 0].set_title(f"Normal Distribution: p-value = {norm_p:.4f}")
+# axs[0, 0].plot(norm_bins, stats.norm.pdf(norm_bins, mu, sigma)*n/bins, lw=2)
+#
+# # Exponential distribution
+# exp_observed, exp_bins, _ = axs[0, 1].hist(data_exp, bins=exp_bins, density=True, alpha=0.5)
+# exp_chi2, exp_p = stats.chisquare(exp_observed, exp_expected)
+# axs[0, 1].set_title(f"Exponential Distribution: p-value = {exp_p:.4f}")
+# axs[0, 1].plot(exp_bins, stats.expon.pdf(exp_bins, 0, beta)*n/bins, lw=2)
+#
+# # Poisson distribution
+# poisson_observed, poisson_bins, _ = axs[1, 0].hist(data_poisson, bins=poisson_bins, density=True, alpha=0.5)
+# poisson_chi2, poisson_p = stats.chisquare(poisson_observed, poisson_expected)
+# axs[1, 0].set_title(f"Poisson Distribution: p-value = {poisson_p:.4f}")
+# axs[1, 0].plot(poisson_bins, stats.poisson.pmf(poisson_bins, lam)*n/bins, lw=2)
+#
+# # Gamma distribution
+# gamma_observed, gamma_bins, _ = axs[1, 1].hist(data_gamma, bins=gamma_bins, density=True, alpha=0.5)
+# gamma_chi2, gamma_p = stats.chisquare(gamma_observed, gamma_expected)
+# axs[1, 1].set_title
+
+
+from flask import Flask, render_template, request
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+# Define routes
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get distribution type from form submission
+        distribution_type = request.form['distribution_type']
+
+        # Generate random sample data for selected distribution
+        if distribution_type == 'normal':
+            data = np.random.normal(loc=0, scale=1, size=100)
+        elif distribution_type == 'exponential':
+            data = np.random.exponential(scale=1, size=100)
+        elif distribution_type == 'poisson':
+            data = np.random.poisson(lam=1, size=100)
+        elif distribution_type == 'gamma':
+            data = np.random.gamma(shape=2, scale=1, size=100)
+
+        # Calculate expected values for selected distribution
+        if distribution_type == 'normal':
+            expected = stats.norm(loc=np.mean(data), scale=np.std(data)).pdf(data)
+        elif distribution_type == 'exponential':
+            expected = stats.expon(scale=np.mean(data)).pdf(data)
+        elif distribution_type == 'poisson':
+            expected = stats.poisson(mu=np.mean(data)).pmf(data)
+        elif distribution_type == 'gamma':
+            expected = stats.gamma(a=np.mean(data)**2/np.var(data), scale=np.var(data)/np.mean(data)).pdf(data)
+
+        # Calculate Chi-square statistic and p-value
+        chisq_stat, p_value = stats.chisquare(f_obs=np.histogram(data, bins='auto')[0], f_exp=expected)
+
+        # Create histogram of sample data and plot expected distribution
+        plt.hist(data, bins='auto', density=True, alpha=0.7)
+        plt.plot(data, expected, 'r-', lw=2)
+        plt.title(f'{distribution_type.capitalize()} Distribution')
+        plt.xlabel('Value')
+        plt.ylabel('Density')
+        plt.savefig('static/plot.png')
+
+        # Render template with plot image and results
+        return render_template('home.html', plot_path='static/plot.png', chisq_stat=chisq_stat, p_value=p_value)
+
+    else:
+        # Render initial form
+        return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ kolmogorov_smirnov.py
@@ -0,0 +1,113 @@
+# import numpy as np
+# import scipy.stats as stats
+# import matplotlib.pyplot as plt
+#
+# # set the random seed for reproducibility
+# np.random.seed(123)
+#
+# # generate data for normal distribution
+# normal_data = np.random.normal(loc=5, scale=2, size=1000)
+#
+# # calculate the mean and standard deviation of the data
+# normal_mean = np.mean(normal_data)
+# normal_std = np.std(normal_data)
+#
+# # generate data for exponential distribution
+# exponential_data = np.random.exponential(scale=2, size=1000)
+#
+# # calculate the mean of the data
+# exponential_mean = np.mean(exponential_data)
+#
+# # generate data for poisson distribution
+# poisson_data = np.random.poisson(lam=3, size=1000)
+#
+# # calculate the mean of the data
+# poisson_mean = np.mean(poisson_data)
+#
+# # generate data for gamma distribution
+# gamma_data = np.random.gamma(shape=2, scale=2, size=1000)
+#
+# # calculate the mean and variance of the data
+# gamma_mean = np.mean(gamma_data)
+# gamma_var = np.var(gamma_data)
+#
+# # plot the normal distribution
+# plt.hist(normal_data, density=True, alpha=0.5, label='data')
+# x = np.linspace(normal_mean - 3*normal_std, normal_mean + 3*normal_std, 100)
+# plt.plot(x, stats.norm.pdf(x, normal_mean, normal_std), label='normal distribution')
+# plt.legend()
+# plt.title('Normal Distribution')
+# plt.show()
+#
+# # plot the exponential distribution
+# plt.hist(exponential_data, density=True, alpha=0.5, label='data')
+# x = np.linspace(0, 10, 100)
+# plt.plot(x, stats.expon.pdf(x, scale=exponential_mean), label='exponential distribution')
+# plt.legend()
+# plt.title('Exponential Distribution')
+# plt.show()
+#
+# # plot the poisson distribution
+# plt.hist(poisson_data, density=True, alpha=0.5, label='data')
+# x = np.arange(0, 15)
+# plt.plot(x, stats.poisson.pmf(x, poisson_mean), 'bo', ms=8, label='poisson distribution')
+# plt.legend()
+# plt.title('Poisson Distribution')
+# plt.show()
+#
+# # plot the gamma distribution
+# plt.hist(gamma_data, density=True, alpha=0.5, label='data')
+# x = np.linspace(0, 15, 100)
+# plt.plot(x, stats.gamma.pdf(x, a=gamma_mean**2/gamma_var, scale=gamma_var/gamma_mean), label='gamma distribution')
+# plt.legend()
+# plt.title('Gamma Distribution')
+# plt.show()
+
+
+from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import expon, gamma, norm, poisson
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get user input
+        distribution_type = request.form['distribution_type']
+        num_samples = int(request.form['num_samples'])
+
+        # Generate data
+        if distribution_type == 'normal':
+            data = norm.rvs(size=num_samples)
+        elif distribution_type == 'uniform':
+            data = np.random.uniform(size=num_samples)
+        elif distribution_type == 'standard':
+            data = np.random.randn(num_samples)
+        elif distribution_type == 'exponential':
+            data = expon.rvs(size=num_samples)
+        elif distribution_type == 'poisson':
+            data = poisson.rvs(mu=5, size=num_samples)
+        elif distribution_type == 'gamma':
+            data = gamma.rvs(a=5, size=num_samples)
+        else:
+            data = None
+
+        # Generate plot
+        if data is not None:
+            plt.hist(data, bins=30)
+            plt.title(f'{distribution_type.capitalize()} Distribution')
+            plt.xlabel('Value')
+            plt.ylabel('Frequency')
+            plt.savefig('static/plot.png')
+
+            # Render template with plot
+            return render_template('index.html', plot_url='static/plot.png')
+        else:
+            return render_template('index.html', error=True)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
